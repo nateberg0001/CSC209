b8e1380fb4670677b63784eda018fdf7fb719c1f
Nathan <Nathan.bergman@mail.utoronto.ca>
2026-03-06 14:45:52 -0500

    done?

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -31,7 +31,51 @@ int main(void) {
       exit(1);
   }
   
-  // TODO
+  // set up the pipes!!
+  int pipefd[2];
+  pipe(pipefd);
 
+  //set up the fork! 
+  pid_t pid = fork();
+
+  if(pid==0){
+    close(pipefd[1]); // no writing needed
+    dup2(pipefd[0], STDIN_FILENO); // however we do want our read pipe to replace stdin
+    close(pipefd[0]); // but not right now, it's for later
+    execl("./validate", "validate", user_id, password, NULL);
+
+    perror("validate failed to reach an exit(i)");
+    exit(127);
+  }
+
+  close(pipefd[0]); // this time no reading required!
+  dprintf(pipefd[1],"%s",user_id); 
+  usleep(10000);
+  dprintf(pipefd[1],"%s",password);
+  // give explicit text to be written to write end of pipe
+  close(pipefd[1]); // done with assigning it so we can close it
+
+  int status;
+  waitpid(pid,&status,0);
+
+  if(WIFEXITED(status)==1){
+    //exited with some code, assigns code to outcome, then proceeds by cases
+    int outcome = WEXITSTATUS(status);
+    if(outcome==0){
+      printf(SUCCESS);
+    }
+    if(outcome==1){
+      exit(0);
+    }
+    if(outcome==2){
+      printf(INVALID);
+    }
+    if(outcome==3){
+      printf(NO_USER);
+    }
+  }
+  else{
+    exit(1);
+  }
   return 0;
 }
